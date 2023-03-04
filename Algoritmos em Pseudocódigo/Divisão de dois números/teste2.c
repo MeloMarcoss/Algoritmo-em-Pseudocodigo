5cf3a6aa05de9d5a689c3f04ff86622879c99976
Marcos José <70660098+MeloMarcoss@users.noreply.github.com>
2023-03-04 22:24:55 +0000

    Um algoritmo para mostrar o resultado da divisão de dois números

---
+++ Algoritmos em Pseudocódigo/Divisão de dois números/teste2.c
@@ -0,0 +1,32 @@
+/*
+Passo 1 - Receber os dois números que serão dividos.
+Passo 2 - Se o segundo número for igual a zero, não poderá haver divisão, pois não existe divisão por zero; caso contrário, dividir os números e mostrar o resultado da divisão.
+
+* Algoritmo em pseudocódigo:
+  ALGORITMO
+  DECLARE N1, N2, D NUMÉRICO
+  ESCREVA "Digite dois números"
+  LEIA N1, N2
+  SE N2 = 0
+  ENTÃO ESCREVA "Impossível dividir"
+  SENÃO INÍCIO
+      D <- N1/N2
+      ESCREVA "Divisão = ", D
+    FIM
+  FIM_ALGORITMO
+*/
+
+#include <stdio.h>
+
+int main() {
+    float N1, N2, D;
+    printf("Digite dois números: ");
+    scanf("%f%f", &N1, &N2);
+    if (N2 == 0) {
+        printf("Impossível dividir\n");
+    } else {
+        D = N1 / N2;
+        printf("Divisão = %.2f\n", D);
+    }
+    return 0;
+}
